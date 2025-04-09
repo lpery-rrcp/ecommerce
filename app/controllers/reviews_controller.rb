64110a5e09955e733c0ac046f1d5d2ac7b866f0f
Lance <lpery@academic.rrc.ca>
2025-04-09 15:52:02 -0500

    Added the create and review_params functions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    product = Product.find(params[:review][:product_id])
+    review = product.reviews.build(review_params.merge(user: current_user))
+    if review.save
+      flash[:notice] = "Thanks for the review."
+    else
+      flash[:alert] = "error in posting the review!"
+    end
+    redirect_to product_path(product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 end
