3af01dadef075a7bfbc6cb032357abed9fa4b823
Lance <lpery@academic.rrc.ca>
2025-04-09 15:02:12 -0500

    Updated the orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @orders = current_user.orders.includes(:order_items)
   end
 
   def show
+    @order = current_user.orders.find(params[:id])
   end
 end
