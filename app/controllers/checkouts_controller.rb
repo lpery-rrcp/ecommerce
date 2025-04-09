6129dbe8c939ea1fa5b6e0c81f653d2a7a74845b
Lance <lpery@academic.rrc.ca>
2025-04-09 11:55:06 -0500

    Updated Checkouts controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,4 +1,16 @@
 class CheckoutsController < ApplicationController
   def create
+    @order = cuttent_user.orders.create(status: :pending)
+    current_cart.each do |product_id, quantity|
+      product = Product.find(product_id)
+      @order.order_items.create!(
+        product: product,
+        quantity: quantity,
+        unit_price: product.price
+      )
+    end
+    @order.update(total_price: @order.order_items.sum { |i| i.quantity * i.unit_price })
+    session[:cart] = {}
+    redirect_to success_cart_path
   end
 end
