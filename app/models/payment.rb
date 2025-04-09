e2323f7c5eb645452eead1df687b2418803f2b24
Lance <lpery@academic.rrc.ca>
2025-04-09 11:48:53 -0500

    Updated associations to Payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :order
   belongs_to :user
+
+  enum status: { pending: 0, completed: 1, failed: 2 }
+  enum payment_method: { credit_card: 0, paypal: 1, bank_transfer: 2 }
 end
