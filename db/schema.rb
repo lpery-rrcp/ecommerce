8b1ca4b0cb750e3dd9291b1cb3632380075e6840
Lance <lpery@academic.rrc.ca>
2025-04-09 11:21:41 -0500

    Added Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_161930) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_162117) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -55,6 +55,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_161930) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -70,4 +79,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_161930) do
 
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users", column: "seller_id"
+  add_foreign_key "reviews", "users"
 end
