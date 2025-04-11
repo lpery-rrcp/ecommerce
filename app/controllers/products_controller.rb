2cca643a8abf1f1695b90afe03d9475aa075cebe
Lance <lpery@academic.rrc.ca>
2025-04-11 17:36:28 -0500

    Added the search and category drop on the product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 class ProductsController < ApplicationController
   def index
     @q = Product.ransack(params[:q])
-    @products = @q.result.includes(:category).page(params[:page])
-    @products = Product.page(params[:page]).per(10)
+
+    # If there's a category filter, filter by it
+    if params[:category_id].present?
+      @products = Product.where(category_id: params[:category_id])
+    else
+      @products = Product.all
+    end
+
+    # If a search keyword is provided, filter by name or description
+    if params[:search].present?
+      @products = @products.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    @products = @products.page(params[:page]).per(10)
   end
 
   def show
