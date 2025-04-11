438b8435e94e274e5552ce5873da48585aeb1dc6
Lance <lpery@academic.rrc.ca>
2025-04-10 23:11:58 -0500

    Added the pagination to teh products and reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,11 @@ class ProductsController < ApplicationController
   def index
     @q = Product.ransack(params[:q])
     @products = @q.result.includes(:category).page(params[:page])
+    @products = Product.page(params[:page]).per(10)
   end
 
   def show
     @product = Product.find(params[:id])
+    @reviews = @product.reviews.page(params[:page]).per(1) # Switch it to 5 later
   end
 end
