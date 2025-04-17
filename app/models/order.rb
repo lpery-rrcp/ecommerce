5f870385ac57e766208e12cb90a3b315d0930424
Lance <lpery@academic.rrc.ca>
2025-04-16 21:58:33 -0500

    Added the validation to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,10 +6,12 @@ class Order < ApplicationRecord
   enum :status, { pending: 0, shipped: 1, delivered: 2, cancelled: 3, paid: 4, unpaid: 5 }
 
   validates :status, presence: true
+  validates :total_price, presence: true, numercality: { greater_than_or_equal_to: 0 }
 
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "id", "id_value", "status", "total_price", "updated_at", "user_id" ]
   end
+
   def self.ransackable_associations(auth_object = nil)
     [ "order_items", "payment", "user" ]
   end
