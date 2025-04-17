f707f1278e0faa56a80e2d705d8f95cff3de5080
Lance <lpery@academic.rrc.ca>
2025-04-16 22:03:08 -0500

    Added the validation to payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,6 +5,9 @@ class Payment < ApplicationRecord
   enum :status, { pending: 0, completed: 1, failed: 2 }
   enum :payment_method, { credit_card: 0, paypal: 1, bank_transfer: 2 }
 
+  validates status:, presence: true
+  validates payment_method, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     [ "id", "order_id", "payment_method", "status", "created_at", "updated_at" ]
   end
