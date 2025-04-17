6f09605debb3690a2967759b13aa2824993e5f54
Lance <lpery@academic.rrc.ca>
2025-04-16 21:54:21 -0500

    Added the validation to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :products
 
-
+  validates :name, presence: true
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "id", "id_value", "name", "updated_at" ]
   end
