b150ad97682474fbabb4b5aaf75902c798bcd94d
Lance <lpery@academic.rrc.ca>
2025-04-16 21:56:54 -0500

    Added the validation to order item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,8 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   def self.ransackable_attributes(auth_object = nil)
     [ "id", "order_id", "product_id", "quantity", "unit_price", "created_at", "updated_at" ]
   end
